246d62b1fd0c323d9f3857d480a4de0d46ab9e57
[Peterson, Briana] <[briana.s.peterson@jpmchase.com]>
2016-01-11 17:38:47 -0500

    call to service for each guess

--- src/scripts/hangman.js
+++ src/scripts/hangman.js
@@ -1,10 +1,25 @@
+'use strict'
 var TitleBox = React.createClass({
-	handleEmailSubmit: function(comment) {
+	handleEmailSubmit: function(comment) { //TODO come up with a better name than comment
 	    $.ajax({
 	      url: this.props.url,
 	      dataType: 'json',
 	      type: 'POST',
 	      data: JSON.stringify(comment),
+	      success: function(data) {
+	        this.setState({data: data});
+	      }.bind(this),
+	      error: function(xhr, status, err) {
+	        console.error(this.props.url, status, err.toString());
+	      }.bind(this)
+	    });
+  	},
+  	handleLetterSubmit: function(comment) { //TODO come up with a better name than comment
+	    $.ajax({
+	      url: this.props.url,
+	      dataType: 'json',
+	      type: 'POST',
+	      data: comment,
 	      success: function(data) {
 	        this.setState({data: data});
 	      }.bind(this),
@@ -21,8 +36,8 @@ var TitleBox = React.createClass({
 			<div className="titleBox">
 				<h2>Hangman</h2>
 				<h3>Enter Your Email Below</h3>
-				<EmailForm onEmailSubmit={this.handleEmailSubmit}/>
-				<NewGameDetails data={this.state.data}/>
+				<EmailForm onEmailSubmit={this.handleEmailSubmit} />
+				<NewGameDetails onLetterSubmit={this.handleLetterSubmit} data={this.state.data} url="http://hangman.coursera.org/hangman/game">
 			</div>
 
 
@@ -60,11 +75,26 @@ var EmailForm = React.createClass({
 });
 
 var NewGameDetails = React.createClass({
-
+	getInitialState: function() {
+    	return {guess: ''};
+  		},
+  	handleSubmit: function(e){
+		e.preventDefault();
+		var guess = this.state.guess; //TODO: sanitize email to make sure its valid format
+		if(!guess){
+			return; //TODO :make something come up when no email
+		}
+		this.props.onLetterSubmit({guess: guess});
+		this.setState({guess: ''});
+	},	
+  	handleLetterChange: function(e) {
+    	this.setState({guess: e.target.value});
+  		},
 	render: function(){
 		var gameData = this.props.data;
 		if (gameData.state === 'alive'){
 		return(
+			<div>
 		<div className="newGameDetails">
 		<h2>New Game Below</h2>
 		<p>{gameData.game_key}</p>
@@ -72,6 +102,11 @@ var NewGameDetails = React.createClass({
 		<p>{gameData.num_tries_left}</p>
 		<p>{gameData.state}</p>
 		</div>
+		<form className="letterForm" onSubmit={this.handleSubmit}>
+				<input type="text" name="guess" placeholder="letter(a-z)" value={this.state.guess} onChange={this.handleLetterChange}/>
+				<input type="submit" value="Post" />
+			</form>
+			</div>
 
 		);
 	}
