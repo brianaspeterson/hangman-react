5f3829daa72fb8837045a65c8d1666b455ca232d
Briana <brianaspeterson@gmail.com>
2016-01-13 22:49:14 -0500

    implemented hot loading and webpack - wow front end ops needs to be a thing

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server',
+    './scripts/hangman.js' // Your appʼs entry point
+  ],
+    devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader:  "babel",
+                query:
+                  {
+                    presets:['es2015', 'react']
+                  }
+            },
+
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                loader: "url?prefix=font/&limit=5000"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            },
+            {
+                test: /\.gif/,
+                loader: "url-loader?limit=10000&mimetype=image/gif"
+            },
+            {
+                test: /\.jpg/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
+            }
+
+             ]
+    },
+    devServer: {
+        contentBase: "./public",
+        noInfo: true, //  --no-info option
+        hot: true,
+        inline: true
+    },
+    plugins: [
+      new webpack.NoErrorsPlugin()
+    ]
+};
